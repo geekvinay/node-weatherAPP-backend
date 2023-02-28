c518671d62f78342207be23690c2e17cf482d07c
geekvinay <geekvinay002@gmail.com>
2023-02-28 23:11:01 +0530

    Added firebase authentication to the project, User's can now use email-password signup

--- src/auth/userAuth.js
+++ src/auth/userAuth.js
@@ -1,9 +1,10 @@
-const { initializeApp } = require('firebase/app');
 const {
   getAuth,
   connectAuthEmulator,
   createUserWithEmailAndPassword,
 } = require('firebase/auth');
+const { initializeApp } = require('firebase/app');
+const axios = require('axios');
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBbLII28LSbOU5l_Bz3JMgSuBWAsyuYn04',
@@ -14,24 +15,32 @@ const firebaseConfig = {
   appId: '1:544896129238:web:95a4eabea1005e17ee9177',
 };
 
+// const email = 'geekvinay0002@gmail.com';
+// const password = 'Vinay@12345';
+
+// connectAuthEmulator(auth, );
+async function setupEmulators(auth) {
+  const authUrl = 'http://localhost:9099';
+  connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+}
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const email = 'geekvinay0002@gmail.com';
-const password = 'Vinay@12345';
+setupEmulators(auth);
 
-// Creating a new user
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
+// // Creating a new user
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
 
-    console.log(user);
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+//     console.log(user);
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
 
-    console.log(Error({ errorCode, errorMessage }));
-  });
+//     console.log(Error({ errorCode, errorMessage }));
+//   });
 
-connectAuthEmulator(auth, 'http://127.0.0.1:9099/');
+module.exports = auth;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 require('./database/mongoose');
+require('./auth/userAuth');
 
 app.use(express.json());
 
-// Check for authorization of the user
-const checkAuth = require('./auth/userAuth');
-app.get('/', checkAuth);
-
 app.get('', (req, res) => {
   res.send(`Hello World!....
-  I'm KIWI, a news API that was developed by VINAY KISHORE`);
+  I'm KIWI, a news project that was developed by VINAY KISHORE`);
 });
 
 // Listening on all routers
 const Updates = require('./routers/updates');
 const News = require('./routers/news');
+const userAuth = require('./routers/login');
 
 app.use(Updates);
 app.use(News);
+app.use(userAuth);
 
 // app.get('*', (req, res) => {
 //   res.status(404).send({
